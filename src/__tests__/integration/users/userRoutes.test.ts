20b0e88be66823fbef4d23dd23f7bc4b40360627
Matheus Carneiro <mtscrn21@gmail.com>
2023-01-10 15:38:23 -0300

    Created all users test

--- src/__tests__/integration/users/userRoutes.test.ts
+++ src/__tests__/integration/users/userRoutes.test.ts
@@ -2,7 +2,7 @@ import  request  from "supertest"
 import { DataSource } from "typeorm"
 import app from "../../../app"
 import { AppDataSource } from "../../../data-source"
-import { mockedUser } from "../../mocks"
+import { mockedUser, mockedUserLogin, mockedUserLogin_2 } from "../../mocks"
 
 
 
@@ -46,5 +46,134 @@ describe("/users", () => {
         expect(response.body).toHaveProperty("message")
         expect(response.status).toBe(401)
     })
+
+    test("DELETE /users/:id -  should not be able to delete another user without authorization",async () => {
+        const userLoginResponse = await request(app).post("/login").send(mockedUserLogin);
+        const secondLoginResponse = await request(app).post("/login").send(mockedUserLogin_2);
+        const UserTobeDeleted = await request(app).get('/users').set("Authorization", `Bearer ${secondLoginResponse.body.token}`)
+
+        const response = await request(app).delete(`/users/${UserTobeDeleted.body[0].id}`).set("Authorization", `Bearer ${userLoginResponse.body.token}`)
+
+        expect(response.body).toHaveProperty("message")
+        expect(response.status).toBe(403)
+    })
+
+    test("DELETE /users/:id -  Must be able to soft delete user",async () => {
+        await request(app).post("/user").send(mockedUser)
+
+        const userLoginResponse = await request(app).post("/login").send(mockedUserLogin)
+        const userTobeDeleted = await request(app).get("/users").set("Authorization", `Bearer ${userLoginResponse.body.token}`)
+
+        const response = await request(app).delete(`/users/${userTobeDeleted.body[0].id}`).set("Authorization", `Bearer ${userLoginResponse.body.token}`)
+        const findUser = await request(app).get("/users").set("Authorization", `Bearer ${userLoginResponse.body.token}`)
+
+        expect(response.status).toBe(204)
+        expect(findUser.body[0].isActive).toBe(false)
+
+    })
+
+    test("DELETE /users/:id -  shouldn't be able to delete user with isActive = false",async () => {
+        await request(app).post("/user").send(mockedUser)
+
+        const userLoginResponse = await request(app).post("/login").send(mockedUserLogin)
+        const userTobeDeleted = await request(app).get("/users").set("Authorization", `Bearer ${userLoginResponse.body.token}`)
+
+        const response = await request(app).delete(`/users/${userTobeDeleted.body[0].id}`).set("Authorization", `Bearer ${userLoginResponse.body.token}`)
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty("message")
+    })
+
+    test("DELETE /users/:id -  should not be able to delete user with invalid id",async () => {
+        await request(app).post("/user").send(mockedUser)
+
+        const userLoginResponse = await request(app).post("/login").send(mockedUserLogin)
+
+        const response = await request(app).delete(`/users/13970660-5dbe-423a-9a9d-5c23b8882dsa`).set("Authorization", `Bearer ${userLoginResponse.body.token}`)
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty("message")
+    })
+
+    test("PATCH /users/:id -  should not be able to update another user without authorization",async () => {
+        const userLoginResponse = await request(app).post("/login").send(mockedUserLogin);
+        const secondLoginResponse = await request(app).post("/login").send(mockedUserLogin_2);
+        const UserToBePach = await request(app).get('/users').set("Authorization", `Bearer ${secondLoginResponse.body.token}`)
+
+        const response = await request(app).patch(`/users/${UserToBePach.body[0].id}`).set("Authorization", `Bearer ${userLoginResponse.body.token}`)
+
+        expect(response.body).toHaveProperty("message")
+        expect(response.status).toBe(401)
+    } )
+
+    test("PATCH /users/:id - should not be able to update user with invalid id",async () => {
+        const newValues = {
+            fantasy_name: "Joana Bezerra",
+            password: "1234567"
+        }
+
+        const userLoginResponse = await request(app).post("/login").send(mockedUserLogin)
+        const token = `Bearer ${userLoginResponse.body.token}`
+
+        const response = await request(app).patch("/users/13970660-5dbe-423a-9a9d-5c23b8882dsa").set("Authorization",token).send(newValues)
+        
+        expect(response.body).toHaveProperty("message")
+        expect(response.status).toBe(404)
+    })
+
+    test("PATCH /users/:id - should not be able to update isActive field value",async () => {
+        const newValues = {
+            isActive: false
+        }
+
+        const userLoginResponse = await request(app).post("/login").send(mockedUserLogin)
+        const token = `Bearer ${userLoginResponse.body.token}`
+
+        const userTobeUpdateRequest = await request(app).get("/users").set("Authorization", token)
+        const userTobeUpdateId = userTobeUpdateRequest.body[0].id
+
+        const response = await request(app).patch(`/users/${userTobeUpdateId}`).set("Authorization",token).send(newValues)
+        
+        expect(response.body).toHaveProperty("message")
+        expect(response.status).toBe(401)
+
+    })
+
+    test("PATCH /users/:id - should not be able to update id field value",async () => {
+        const newValues = {
+            id: false
+        }
+
+        const userLoginResponse = await request(app).post("/login").send(mockedUserLogin)
+        const token = `Bearer ${userLoginResponse.body.token}`
+
+        const userTobeUpdateRequest = await request(app).get("/users").set("Authorization", token)
+        const userTobeUpdateId = userTobeUpdateRequest.body[0].id
+
+        const response = await request(app).patch(`/users/${userTobeUpdateId}`).set("Authorization",token).send(newValues)
+        
+        expect(response.body).toHaveProperty("message")
+        expect(response.status).toBe(401)
+    })
+
+    test("PATCH /users/:id -  should be able to update user",async () => {
+        const newValues = {
+            fantasy_name: "Joana LTDA",
+            password: "1234567"
+        }
+
+        const userLoginResponse = await request(app).post("/login").send(mockedUserLogin)
+        const token = `Bearer ${userLoginResponse.body.token}`
+
+        const userTobeUpdateRequest = await request(app).get("/users").set("Authorization", token)
+        const userTobeUpdateId = userTobeUpdateRequest.body[0].id
+
+        const response = await request(app).patch(`/users/${userTobeUpdateId}`).set("Authorization",token).send(newValues)
+
+        const userUpdated = await request(app).get("/users").set("Authorization", token)
+        
+        expect(userUpdated.body[0].fantasy_name).toEqual("Joana LTDA")
+        expect(userUpdated.body[0]).not.toHaveProperty("password")
+        expect(response.status).toBe(200)
+    })
+
 
 })
--- src/__tests__/mocks/index.ts
+++ src/__tests__/mocks/index.ts
@@ -1,15 +1,17 @@
 export const mockedUser = {
     fantasy_name: "Joana",
-    email: "joana@mail.com",
     password: "123456",
+    email: "joana@mail.com",
     cnpj: "13043000000100"
 
 }
 
-export const mockedAdmin = {
-    name: "Felipe",
-    email: "felipe@mail.com",
-    password: "123456"
+export const mockedUser_2 = {
+    fantasy_name: "Mario",
+    email: "mario@mail.com",
+    password: "123456",
+    cnpj: "14043000000100"
+
 }
 
 export const mockedUserLogin = {
@@ -17,7 +19,50 @@ export const mockedUserLogin = {
     password: "123456"
 }
 
-export const mockedAdminLogin = {
-    email: "felipe@mail.com",
+export const mockedUserLogin_2 = {
+    email: "mario@mail.com",
     password: "123456"
 }
+
+export const newClient = {
+    name: "Jonas",
+    document: "11122233345",
+    email: "jonas@mail.com",
+    number: "00912345678"
+}
+
+export const newProduct = {
+    name: "produto_1",
+    value: "10",
+    stock: "5"
+}
+
+export const newSale = {
+    client: "Jonas",
+    product: "produto_1",
+    amout: 5
+}
+
+/* 
+    Users:
+        CNPJ
+        Password
+        Email
+        CommercialName
+
+    Clients:
+        Name
+        Document
+        Email
+        Number
+
+    Products:
+        Name
+        Value
+        Stock
+
+    Sales:
+        client_id
+        product_id
+        amout
+*/
